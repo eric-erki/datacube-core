1cc545ab7968c18647ca5de6af62372585120900
Damien Ayers <damien@omad.net>
2017-05-12 09:11:55 +1000

    Add CLI for viewing embedded NetCDF Dataset records

---
+++ datacube_apps/view_nc_metadata.py
@@ -0,0 +1,47 @@
+#!/bin/env python3
+"""
+Display embedded metadata from a Data Cube style NetCDF file.
+
+
+"""
+import json
+
+import click
+import xarray as xr
+import yaml
+
+from datacube.compat import StringIO
+
+
+@click.command()
+@click.argument('filename')
+@click.option('--yaml', 'output_format', flag_value='yaml', default=True)
+@click.option('--json', 'output_format', flag_value='json')
+@click.option('--time', type=int)
+def main(filename, output_format, variable_name='dataset', time=0):
+    ds = xr.open_dataset(filename)
+
+    document = bytes(ds.data_vars[variable_name].data).decode('utf8')
+
+    if output_format == 'json':
+        obj = yaml.load(StringIO(document))
+        document = json.dumps(obj, indent=4)
+
+    output = format_document(document, output_format)
+    click.echo_via_pager(output)
+
+
+def format_document(document, language):
+    try:
+        from pygments.lexers import get_lexer_by_name
+        from pygments.formatters.terminal import TerminalFormatter
+        from pygments import highlight
+
+        lexer = get_lexer_by_name(language)
+        return highlight(document, lexer, TerminalFormatter(bg='dark'))
+    except ImportError:
+        return document
+
+
+if __name__ == '__main__':
+    main()
